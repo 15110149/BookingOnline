0069c67807e7102642e79903a5f9cdd390731166
Tri Nha <lychautrinha@gmail.com>
2018-12-21 19:35:39 +0700

    update

--- src/component/home_page/About_Top.js
+++ src/component/home_page/About_Top.js
@@ -1,119 +1,236 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
-import { Modal, Row, Button, Form, Select,Radio } from "antd";
-import 'antd/dist/antd.css';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
-import CustomerDatePicker from "../CustomDatePicker/index.js"
-import CustomInput from "../CustomInput/index.js"
-import DropdownButton from "../DropdownButton/index.js"
-const FormItem = Form.Item;
-function mapStateToProps(state) {
-    return {
+import axios from "axios";
+import {
+  Modal,
+  Row,
+  Button,
+  Form,
+  Select,
+  Radio,
+  DatePicker,
+  message
+} from "antd";
+import "antd/dist/antd.css";
+import CustomInput from "../CustomInput/index.js";
+import ModalDatPhong from "./ModalDatPhong";
 
-    };
-}
+const FormItem = Form.Item;
+
+message.config({
+  top: 100
+});
 
 class About_Top extends React.Component {
- 
-    constructor(props) {
-        super(props);
-        this.state = {
-        //   LoaiKhachHang: "",
-        //   GioiTinh: true,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      LoaiPhong: [],
+      dataphongtrong: null,
+      valueLoaiPhong: null,
+      visibleModal: true,
+      loadingCheckPhongTrong: false,
+      NgayDi: "",
+      NgayDen: ""
+    };
+  }
+
+  handleFetchLoaiPhong = async () => {
+    const res = await axios.get(
+      "https://mighty-coast-18749.herokuapp.com/api/loaiphong"
+    );
+    if ((res.status = 200)) {
+      this.setState({ LoaiPhong: res.data });
+    }
+    console.log(this.state);
+  };
+
+  handleChangeLoaiPhong = value => {
+    console.log(value);
+    this.setState({ valueLoaiPhong: value });
+  };
+
+  handleRenderSelectLoaiPhong = data =>
+    data.map(item => {
+      return (
+        <Select.Option name="LoaiPhong" value={[item._id, item.TenLoaiPhong]}>
+          {item.TenLoaiPhong}
+        </Select.Option>
+      );
+    });
+
+  handleCheckPhongTrong = async e => {
+    const { NgayDi, NgayDen, valueLoaiPhong } = this.state;
+    console.log(new Date(NgayDi).getTime() < new Date(NgayDen).getTime());
+    if (NgayDen === "" || NgayDi === "" || valueLoaiPhong === null) {
+      console.log(this.state);
+      message.error("Vui lòng điền đầy đủ thông tin để tìm phòng");
+    } else if (new Date(NgayDi).getTime() < new Date(NgayDen).getTime()) {
+      message.error("Ngày đi phải lớn hơn ngày đến ");
+    } else {
+      this.setState({ loadingCheckPhongTrong: true });
+      const res = await axios.post(
+        "https://mighty-coast-18749.herokuapp.com/api/phong/phongtrong",
+        {
+          LoaiPhong: valueLoaiPhong[0],
+          NgayDi,
+          NgayDen
+        }
+      );
+      if ((res.status = 200)) {
+        console.log("res 200");
+        this.setState({
+          dataphongtrong: res.data,
+          loadingCheckPhongTrong: false
+        });
+        if (this.state.dataphongtrong.length === 0) {
+          message.error("Hiện tại không có phòng cho loại phòng này");
+        } else {
+          message.success("Tìm thấy phòng");
+          this.setState({ visibleModal: true });
+        }
       }
-    
-    render() {
-        const formItemLayout = {
-            labelCol: {
-              xs: { span: 24 },
-              sm: { span: 4 }
-            },
-            wrapperCol: {
-              xs: { span: 24 },
-              sm: { span: 20 }
-            }
-          };
-        return (
-            <div>
-                <section id="about" className="about">
-                    <div className="container">
-                        <div className="row title-bar">
-                            <div className="col-md-12">
-                                <h1 className="wow fadeInUp">BOOKING NOW</h1>
-                                <div className="row">   
-                                   <div className="col-md-3 col-sm-8 wow fadeInDown">
-                                      <div className="career-p3-cont text-center">
-                                    
-                                        <FormItem label="Ngày Đến" className="career-p3-cont text-center">
-                                            <Field
-                                                name="NgayDen" {...this.formItemLayout}
-                                                type="text"
-                                                component={CustomerDatePicker}
-                                             />
-                                        </FormItem>
-                                       
-                                      </div>
-                                   </div>
+      console.log(this.state);
+    }
+    e.preventDefault();
+  };
 
-                                   <div className="col-md-3 col-sm-8 wow fadeInDown">
-                                      <div className="career-p3-cont text-center">
-                                    
-                                        <FormItem label="Ngày Đi" >
-                                            <Field
-                                                name="NgayDen" {...this.formItemLayout}
-                                                type="text"
-                                                component={CustomerDatePicker}
-                                             />
-                                        </FormItem>
-                                       
-                                      </div>
-                                   </div>
+  onChangeNgayDi = (date, dateString) => {
+    this.setState({ NgayDi: dateString });
+    console.log(dateString);
+  };
 
-                                    <div className="col-md-3 col-sm-8 wow fadeInDown">
-                                      <div className="career-p3-cont text-center">
-                                    
-                                        <FormItem label="Loại Phòng" >
-                                            <Field
-                                                name="LoaiPhong" {...this.formItemLayout}
-                                                type="text"
-                                                component={DropdownButton}
-                                             />
-                                        </FormItem>
-                                       
-                                      </div>
-                                   </div>
+  onChangeNgayDen = (date, dateString) => {
+    this.setState({ NgayDen: dateString });
+    console.log(dateString);
+  };
 
+  handleCancel = () => {
+    this.setState({ visibleModal: false, NgayDi: "", NgayDen: "", });
+  };
 
-                                <div className="col-md-3 col-sm-8 wow fadeInDown">
-                                      <div className="career-p3-cont text-center">
-                                    <div style={{paddingTop: '10%'}}>
-                                      <button className="btn btn-general btn-white" role="button"  >
-                                      <Link to="/news" className="nav-link smooth-scroll"  style={{color : 'rgb(243, 181, 9)'}}>SEARCH</Link>
-                                        </button>
-                                    </div>
-                                      </div>
-                                   </div>
+  componentDidMount = () => {
+    this.handleFetchLoaiPhong();
+  };
 
+  render() {
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 4 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 20 }
+      }
+    };
+    console.log("State ", this.state);
+    return (
+      <div>
+        <section id="about" className="about">
+          <div className="container">
+            <div className="row title-bar">
+              <div className="col-md-12">
+                <h1 className="wow fadeInUp">BOOKING NOW</h1>
+                <Form onSubmit={values => this.handleCheckPhongTrong(values)}>
+                  <div className="row">
+                    <div className="col-md-3 col-sm-8 wow fadeInDown">
+                      <div className="career-p3-cont text-center">
+                        <FormItem
+                          label="Ngày Đến"
+                          className="career-p3-cont text-center"
+                        >
+                          <DatePicker
+                            format="MM/DD/YYYY"
+                            onChange={this.onChangeNgayDen}
+                          />
+                        </FormItem>
+                      </div>
+                    </div>
 
+                    <div className="col-md-3 col-sm-8 wow fadeInDown">
+                      <div className="career-p3-cont text-center">
+                        <FormItem label="Ngày Đi">
+                          <DatePicker
+                            format="MM/DD/YYYY"
+                            onChange={this.onChangeNgayDi}
+                          />
+                        </FormItem>
+                      </div>
+                    </div>
 
-                                </div>
-                               
-                            </div>
-                            {/* <div style={{display: 'flex', marginLeft:'90%'}}>
+                    <div className="col-md-3 col-sm-8 wow fadeInDown">
+                      <div className="career-p3-cont text-center">
+                        <FormItem label="Loại Phòng">
+                          <Select
+                            name="LoaiPhong"
+                            style={{ width: 200 }}
+                            onChange={this.handleChangeLoaiPhong}
+                          >
+                            {this.state.LoaiPhong.length > 0
+                              ? this.handleRenderSelectLoaiPhong(
+                                  this.state.LoaiPhong
+                                )
+                              : console.log("Không có list")}
+                          </Select>
+                        </FormItem>
+                      </div>
+                    </div>
+                    {/* <Button
+                      type="primary"
+                      htmlType="submit"
+                      className="button"
+                      onClick={e=> e.preventDefault()}
+                      //   loading={adddatphong.isFetching} // true
+                      //   disabled={adddatphong.isFetching}
+                    >
+                      Thêm
+                    </Button> */}
+                    <div className="col-md-3 col-sm-8 wow fadeInDown">
+                      <div className="career-p3-cont text-center">
+                        <div style={{ paddingTop: "28%" }}>
+                          <Button
+                            className="btn btn-general btn-white"
+                            role="button"
+                            onClick={this.handleCheckPhongTrong}
+                            loading={this.state.loadingCheckPhongTrong}
+                            disabled={this.state.loadingCheckPhongTrong}
+                          >
+                            {/* <Link
+                              to="/news"
+                              className="nav-link smooth-scroll"
+                              style={{ color: "rgb(243, 181, 9)" }}
+                            > */}
+                            SEARCH
+                            {/* </Link> */}
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Form>
+              </div>
+              {/* <div style={{display: 'flex', marginLeft:'90%'}}>
                            <button className="btn btn-general btn-white" role="button"  >
                             <Link to="/news" className="nav-link smooth-scroll"  style={{color : 'rgb(90, 194, 21)'}}>SUBMIT</Link>
                             </button>
                      </div> */}
-                            </div>
-                   
-                        </div>
-                    
-                </section>
             </div>
-        );
-    }
+          </div>
+        </section>
+        <ModalDatPhong
+          dataphongtrong={this.state.dataphongtrong}
+          LoaiPhong={this.state.valueLoaiPhong}
+          NgayDen={this.state.NgayDen}
+          NgayDi={this.state.NgayDi}
+          visible={this.state.visibleModal}
+          onCancel={this.handleCancel}
+          onOk={this.handleOk}
+          {...this.props}
+        />
+      </div>
+    );
+  }
 }
 
 // export default connect(
@@ -121,8 +238,8 @@ class About_Top extends React.Component {
 // )(About_Top);
 
 About_Top = reduxForm({
-    form: 'editForm' ,
-   //   validate, // <--- validation function given to redux-form
-    forceUnregisterOnUnmount: true,
-  })(About_Top)
-  export default About_Top;
+  form: "editForm",
+  //   validate, // <--- validation function given to redux-form
+  forceUnregisterOnUnmount: true
+})(About_Top);
+export default About_Top;
---
+++ src/component/home_page/ModalDatPhong/index.js
@@ -0,0 +1,184 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import { Modal, Row, Button, Form, Select, message, Input } from "antd";
+import CustomInput from "../../../component/CustomInput";
+import CustomDatePicker from "../../../component/CustomDatePicker";
+import validate from "./validate";
+import axios from "axios";
+
+const FormItem = Form.Item;
+
+class ModalDatPhong extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loadingDatPhong: false,
+      KhachHang: "",
+      Phong: "",
+      SoNguoi: "",
+      HoTen: "",
+      Email: "",
+      DienThoai: ""
+    };
+  }
+
+  onChangeSoNguoi = e => {
+    this.setState({ SoNguoi: e.target.value });
+  };
+
+  onChangeHoTen = e => {
+    this.setState({ HoTen: e.target.value });
+  };
+
+  onChangeEmail = e => {
+    this.setState({ Email: e.target.value });
+  };
+
+  onChangeDienThoai = e => {
+    console.log(this.state);
+    this.setState({ DienThoai: e.target.value });
+  };
+
+  formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 4 }
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 20 }
+    }
+  };
+
+  checkvalidate = () => {
+    const { SoNguoi, HoTen, Email, DienThoai } = this.state;
+    if (SoNguoi === "" || HoTen === "" || DienThoai === "" || Email === "") {
+      message.error("Không được bỏ trống ô");
+      return false;
+    }
+  };
+
+  handleDatPhong = async () => {
+    const { SoNguoi, HoTen, Email, DienThoai } = this.state;
+    const { NgayDi, NgayDen, dataphongtrong, onCancel } = this.props;
+    if (this.checkvalidate() !== false) {
+      const res = await axios.post(
+        "https://mighty-coast-18749.herokuapp.com/api/online",
+        {
+          Phong: dataphongtrong._id,
+          NgayDi,
+          SoNguoi,
+          NgayDen,
+          HoTen,
+          Email,
+          SoDienThoai: DienThoai
+        }
+      );
+      if ((res.status = 200)) {
+        message.success("Đặt phòng thành công");
+        this.setState({
+          KhachHang: "",
+          Phong: "",
+          SoNguoi: "",
+          HoTen: "",
+          Email: "",
+          DienThoai: ""
+        });
+        onCancel();
+      }
+    }
+  };
+
+  render() {
+    const {
+      NgayDen,
+      NgayDi,
+      handleSubmit,
+      visible,
+      onCancel,
+      LoaiPhong
+    } = this.props;
+    const { SoNguoi, HoTen, Email, DienThoai } = this.state;
+    console.log("Modal dat phong ", this.props);
+    console.log("Visible dat phong ", visible);
+    return (
+      <Modal
+        title="Đặt phòng"
+        visible={visible}
+        onCancel={() => {
+          onCancel();
+        }}
+        footer={null}
+      >
+        <Form
+          onSubmit={handleSubmit(values =>
+            this.handleDatPhong(values, onCancel)
+          )}
+        >
+          <FormItem label="Ngày đến" {...this.formItemLayout}>
+            {NgayDen}
+          </FormItem>
+
+          <FormItem label="Ngày đi" {...this.formItemLayout}>
+            {NgayDi}
+          </FormItem>
+          <FormItem label="Loại Phòng" {...this.formItemLayout}>
+            {LoaiPhong !== null ? LoaiPhong[1] : null}
+          </FormItem>
+          <FormItem label="Số người" {...this.formItemLayout}>
+            <Input
+              placeholder="Nhập số người"
+              value={SoNguoi}
+              onChange={this.onChangeSoNguoi}
+              //   ref={node => (this.userNameInput = node)}
+            />
+          </FormItem>
+          <FormItem label="Họ tên" {...this.formItemLayout}>
+            <Input
+              placeholder="Nhập Họ tên"
+              value={HoTen}
+              onChange={this.onChangeHoTen}
+              //   ref={node => (this.userNameInput = node)}
+            />
+          </FormItem>
+          <FormItem label="Email" {...this.formItemLayout}>
+            <Input
+              placeholder="Nhập Email"
+              value={Email}
+              onChange={this.onChangeEmail}
+              //   ref={node => (this.userNameInput = node)}
+            />
+          </FormItem>
+          <FormItem label="Điện thoại" {...this.formItemLayout}>
+            <Input
+              placeholder="Nhập điện thoại"
+              value={DienThoai}
+              onChange={this.onChangeDienThoai}
+              //   ref={node => (this.userNameInput = node)}
+            />
+          </FormItem>
+
+          {/* <Field name="age" type="number" component={renderField} label="Age" /> */}
+          <Row type="flex" justify="end">
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="button"
+              //   loading={adddatphong.isFetching} // true
+              //   disabled={adddatphong.isFetching}
+            >
+              Đặt phòng
+            </Button>
+          </Row>
+        </Form>
+      </Modal>
+    );
+  }
+}
+
+ModalDatPhong = reduxForm({
+  form: "dat-phong", // a unique identifier for this form
+  validate // <--- validation function given to redux-form
+})(ModalDatPhong);
+
+export default ModalDatPhong;
